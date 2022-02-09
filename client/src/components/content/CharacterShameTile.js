2245363aa6688dce722c0e77f8a0c929a71210d3
Denny Wright <dennison.wright89@gmail.com>
2022-02-09 17:01:14 -0500

    Ohad changes adressed

--- client/src/components/content/CharacterShameTile.js
+++ client/src/components/content/CharacterShameTile.js
@@ -2,16 +2,16 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 const CharacterShameTile = props => {
-  const { id } = props.character
+  const { id, name, sum, pictureUrl } = props.character
 
   return (
     <div className="shameTile centered">
       <h2>SHAME ZONE</h2>
       <h4>#1 Loser</h4>
       <Link to={`/characters/${id}`}>
-        <p>{props.character.name} - Total votes: {props.character.sum}</p>
+        <p>{name} - Total votes: {sum}</p>
         <div className="shame centered">
-          <img className="loser" src={props.character.pictureUrl}/>
+          <img className="loser" src={pictureUrl}/>
           <br />
           <img src="https://i.imgur.com/WCzRK16.jpg"/>
         </div>
